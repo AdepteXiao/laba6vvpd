9960bd8a5f9095d783221e90e2ff4dc934f6935f
ArinaD <ademkhina@list.ru>
2021-12-22 16:54:05 +0700

    Added max func

--- vvpd4.py
+++ vvpd4.py
@@ -7,4 +7,15 @@ def syracuse_sequence(n: int):
             n = n * 2 + 1
         result.append(n)
     return result
-    
+
+
+def syracuse_max(n: int):
+    maxim = n
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 2 + 1
+        if n > maxim:
+            maxim = n
+    return maxim
