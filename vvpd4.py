f15c6d5969df2a3c41c679f4aa34074d302e7b2f
ArinaD <ademkhina@list.ru>
2021-12-22 16:41:01 +0700

    Added syracuse func

--- vvpd4.py
+++ vvpd4.py
@@ -1,2 +1,10 @@
-def main():
+def syracuse_sequence(n: int):
+    result = []
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 2 + 1
+        result.append(n)
+    return result
     
