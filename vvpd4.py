54305ca8900f55107b716dbe9d04788e2cf171ec
ArinaD <ademkhina@list.ru>
2021-12-22 17:24:09 +0700

    debugged version

--- vvpd4.py
+++ vvpd4.py
@@ -1,10 +1,10 @@
 def syracuse_sequence(n: int):
-    result = []
+    result = [n]
     while n != 1:
         if n % 2 == 0:
             n = n // 2
         else:
-            n = n * 2 + 1
+            n = n * 3 + 1
         result.append(n)
     return result
 
@@ -15,7 +15,26 @@ def syracuse_max(n: int):
         if n % 2 == 0:
             n = n // 2
         else:
-            n = n * 2 + 1
+            n = n * 3 + 1
         if n > maxim:
             maxim = n
     return maxim
+
+
+def main():
+    print('Для числа 324:')
+    print('Сиракузская последовательность: ', syracuse_sequence(324))
+    print('Максимальное число в этой последовательности: ', syracuse_max(324))
+    print('Для числа 1233:')
+    print('Сиракузская последовательность: ', syracuse_sequence(1233))
+    print('Максимальное число в этой последовательности: ', syracuse_max(1233))
+    print('Для числа 11:')
+    print('Сиракузская последовательность: ', syracuse_sequence(11))
+    print('Максимальное число в этой последовательности: ', syracuse_max(11))
+    print('Для числа 7:')
+    print('Сиракузская последовательность: ', syracuse_sequence(7))
+    print('Максимальное число в этой последовательности: ', syracuse_max(7))
+
+
+if __name__ == '__main__':
+    main()
